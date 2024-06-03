3bada3717e7b03d48b95a9d6002bdb2e88959acc
João Vinicius de Aquino Farias <131161400+ApolloMorningstar@users.noreply.github.com>
2024-06-03 12:22:53 -0300

    commit spinner_pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
 import './App.css'
 import Pokedex from './components/Pokedex'
+import React, {useState, useEffect} from 'react'
+import style from 'styled-components'
+import {motion} from "framer-motion"
 
+const Spinner = style(motion.div)`
+  width: 50px;
+  height: 50px;
+  border: 4px solid black;
+  border-top: 4px solid red;
+  border-radius: 50%;
+  display: inline-block;
+`;
+
+const Container = style.div`
+  display: flex;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 function App() {
-
-  return (
-    <>
-      <Pokedex />
-    </>
-  )
+  const [carregando, setCarregando] = useState(true);
+  useEffect(() => {
+    const tempo = setTimeout(() =>{
+      setCarregando(false);
+    }, 3000)
+    return () => clearTimeout(tempo);
+  },[])
+  return (<Container>
+    {carregando ? (
+    <Spinner
+    animate={{rotate: 360}}
+    transition={{duration: 0.01, repeat: Infinity, ease: 'linear'}}
+    ></Spinner>
+    ):(
+      <Pokedex/>
+    )}
+  </Container>
+)
 }
 
 export default App
